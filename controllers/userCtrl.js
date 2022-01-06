8018da288ed1aee1e80f5df2a54abe5ab1f2e072
TungSmartPhan <tungptgcd18390@fpt.edu.vn>
2022-01-07 04:08:17 +0700

    Create Register Form Controller

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -1,14 +1,48 @@
 const Users = require('../models/userModel')
-
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 const userCtrl = {
     register:  async (req, res) => {
         try {
+            const {name, email, password} = req.body;
+
+            if(!name || !email || !password){
+                return res.status(400).json({message:"Please fill in all fields."})
+            }
+
+            if(!validateEmail(email)){
+                return res.status(400).json({message:"This email is unavailable. !!!"})
+            }
+
+            const user = await User.findOne({email})
+            if(user) {
+                return res.status(400).json({message:"This email is already exists. !!!"})
+            }
+
+            if(password.length < 6 )
+            { 
+                return res.status(400).json({message:"Password must be at least 6 characters"})
+            }
+
+           
+
+            const newUser = {
+                name , email, password: passwordHash
+            }
+
+            
+
             res.json({ message:"Register Test"})
         } catch (error) {
             return res.status(500).json({ message: error.message})
         }
     }
+}
+
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
 }
 
 module.exports = userCtrl
