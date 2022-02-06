811bf415ac4fc425e9ea01961316a0d40d6f4d45
TungSmartPhan <tungptgcd18390@fpt.edu.vn>
2022-02-06 16:26:40 +0700

    Fix Some Bug from auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,12 +2,12 @@ const jwt = require("jsonwebtoken");
 const auth = (req, res, next) => {
   try {
     //check access token
-    const token = req.headers("Authorization");
-    if (!token) res.status(500).json({ message: "Authentication failed" });
+    const token = req.header("Authorization");
+    if (!token) return  res.status(400).json({ message: "Authentication failed" });
     // validation access token
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET),
-      (err, user) => {
-        if (err) res.status(400).json({ message: "Authentication failed" });
+      (error, user) => {
+        if (error) return res.status(400).json({ message: "Authentication failed" });
         // success
         req.user = user;
         next();
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const userCtrl = require('../controllers/userCtrl')
+const auth = require('../middleware/auth')
 
 router.post('/register', userCtrl.register)
 router.post('/register/activation', userCtrl.activateEmail)
 router.post('/login', userCtrl.login)
 router.post('/refresh_token', userCtrl.getAccessToken)
 router.post('/forgot',userCtrl.forgotPassword)
+router.post('/reset', auth,  userCtrl.resetPassword)
 module.exports = router;
